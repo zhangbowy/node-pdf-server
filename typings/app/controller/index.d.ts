413909aaed1aade10dbb041b6a8dcb340e3c1258
zhangbowy <1751170889@qq.com>
2021-09-03 10:17:23 +0800

    feat: 相关修改

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -1,13 +1,11 @@
-// This file is created by egg-ts-helper@1.25.9
+// This file is created by egg-ts-helper@1.26.0
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportBase from '../../../app/controller/base';
 import ExportHome from '../../../app/controller/home';
 
 declare module 'egg' {
   interface IController {
-    base: ExportBase;
     home: ExportHome;
   }
 }
