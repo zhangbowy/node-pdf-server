83d7f427f2da6a7952e54ccf51dd3a688223b234
zhangbowy <1751170889@qq.com>
2021-09-03 11:17:48 +0800

    fix: 配置

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -2,16 +2,10 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportBase from '../../../app/core/base';
 import ExportHome from '../../../app/controller/home';
-import ExportAdminHome from '../../../app/controller/admin/home';
 
 declare module 'egg' {
   interface IController {
-    base: ExportBase;
     home: ExportHome;
-    admin: {
-      home: ExportAdminHome;
-    }
   }
 }
--- typings/app/middleware/index.d.ts
+++ typings/app/middleware/index.d.ts
@@ -4,14 +4,12 @@
 import 'egg';
 import ExportAuth from '../../../app/middleware/auth';
 import ExportErrorHandler from '../../../app/middleware/errorHandler';
-import ExportSpecial from '../../../app/middleware/special';
 import ExportUuid from '../../../app/middleware/uuid';
 
 declare module 'egg' {
   interface IMiddleware {
     auth: typeof ExportAuth;
     errorHandler: typeof ExportErrorHandler;
-    special: typeof ExportSpecial;
     uuid: typeof ExportUuid;
   }
 }
