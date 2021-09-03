0626618413a78a12d663049c48564c417b7913a9
zhangbowy <1751170889@qq.com>
2021-09-03 18:52:12 +0800

    test: schema

--- app/schema/order_rule.ts
+++ app/schema/order_rule.ts
@@ -1,5 +1,38 @@
 import { Application } from 'egg';
 
 export default (app: Application) => {
-  return {app};
+  const { Sequelize } = app;
+  return {
+    id: {
+      type: Sequelize.INTEGER().UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: "主键",
+    },
+    username: {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+      comment: "用户名称",
+    },
+    type: {
+      type: Sequelize.INTEGER().UNSIGNED,
+      allowNull: false,
+      default: 1,
+      comment: "类型:1小学生，2中学生,3高中生",
+    },
+    status: {
+      type: Sequelize.BOOLEAN, // 1返回true，0返回false
+      allowNull: false,
+      default: 1,
+      comment: "状态值:1启用 0 关闭",
+    },
+    createdTime: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updateTime: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  };
 };
