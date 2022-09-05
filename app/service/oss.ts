8aa181ff5b99c75294b5351538511540524d87d5
zhangbo <1751179889@qq.com>
2022-09-05 12:32:28 +0800

    fix: bug

--- app/service/oss.ts
+++ app/service/oss.ts
@@ -2,17 +2,21 @@ import { Service } from 'egg';
 const OSS = require('ali-oss');
 const _ = require('lodash');
 
-const client = new OSS({
-    // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
-    region: '',
-    // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    accessKeyId: '',
-    accessKeySecret: '',
-    endpoint: '',
-    bucket: ''
-});
+let client: any = null;
 
 export default class OssService extends Service {
+    constructor(props) {
+        super(props);
+        client = new OSS({
+            // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
+            region: '',
+            // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+            accessKeyId: '',
+            accessKeySecret: '',
+            endpoint: '',
+            bucket: ''
+        });
+    }
     /**
      * 前缀
      */
